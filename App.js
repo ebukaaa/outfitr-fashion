a5381e83ac4ec3f442479e8c0aee342576f3fb52
Ebuks <ebukaa@protonmail.com>
2022-07-25 23:10:54 +0100

    modified files

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
-import "react-native-gesture-handler";
-import { useStore } from "./utils";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { setStatusBarStyle } from "expo-status-bar";
+import { useLayoutEffect } from "react";
+import { Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Screens from "./screens";
+import "styles/app.scss";
 
 export default function App() {
-  const { isLoaded, StatusBar, Text, NavigationContainer, Screens } =
-    useStore();
+  const [isLoaded, isError] = useFonts({
+    SFProDisplayBold: require("assets/fonts/sf-pro-display/Bold.otf"),
+    SFProDisplayMedium: require("assets/fonts/sf-pro-display/Medium.otf"),
+    SFProDisplayRegular: require("assets/fonts/sf-pro-display/Regular.otf"),
+    SFProDisplaySemibold: require("assets/fonts/sf-pro-display/Semibold.otf"),
+  });
 
-  return !isLoaded ? (
-    <Text>Loading fonts</Text>
-  ) : (
-    <NavigationContainer>
-      <StatusBar />
-      <Screens />
-    </NavigationContainer>
+  useLayoutEffect(() => {
+    setStatusBarStyle("dark");
+  }, []);
+
+  if (isError) return <Text styleName="message">Error loading fonts</Text>;
+  if (!isLoaded) return <Text styleName="message">Loading fonts</Text>;
+  return (
+    <GestureHandlerRootView styleName="app">
+      <NavigationContainer>
+        <Screens />
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
